bfbd9af53dd603a970ed7ec5ed621be16d157599
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-08 23:18:54 +0700

    tugas_24_js
    tugas sudah ok !!!

---
+++ tugas-24.js
@@ -0,0 +1,12 @@
+function panggilsort (){
+
+  var angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Nilai Sebelumnya: ", angka );
+
+  hasil1 = angka.sort();
+  console.log("Nilai Ascending: ", hasil1);
+
+  hasil2 = angka.reverse();
+  console.log("Nilai Descending: ", hasil2);
+}
+panggilsort();
